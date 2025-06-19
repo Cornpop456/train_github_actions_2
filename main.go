9f2f4fb0d06ebc290c1982e9121bfef7f6e84dd7
Victor Ginzburg <v-viktor456@mail.ru>
2025-06-19 12:54:16 +0300

    new workflow

--- main.go
+++ main.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
 func main() {
-	var wg sync.WaitGroup
-	var shared int
 
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
-		go func() {
-			shared++ // Вот здесь будет гонка на запись
-			wg.Done()
-		}()
-	}
-
-	wg.Wait()
-	fmt.Println("Shared =", shared)
 }
 
 func MaxInt(a, b int) int {
