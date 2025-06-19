05026d575b7cd344cc6684349d81bacb711ca850
Victor Ginzburg <v-viktor456@mail.ru>
2025-06-19 12:47:17 +0300

    real racer

--- main.go
+++ main.go
@@ -7,16 +7,18 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
-	wg.Add(5)
+	var shared int
 
 	for i := 0; i < 5; i++ {
+		wg.Add(1)
 		go func() {
-			fmt.Println(i)
+			shared++ // Вот здесь будет гонка на запись
 			wg.Done()
 		}()
 	}
 
 	wg.Wait()
+	fmt.Println("Shared =", shared)
 }
 
 func MaxInt(a, b int) int {
